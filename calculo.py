a834f313bc04842757fe8bf6d51ecf5208f4ab75
Aluno DevSecOps <aluno@bancocidadao.lab>
2026-09-04 16:55:16 -0300

    Atualiza tratamento de erro

--- calculo.py
+++ calculo.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env  python3
-
-
+#!/usr/bin/env python3
 
 notas = []
 soma = 0
 
-for nota in range(1,6):
-    nota2 = float(input(f"Digite a nota {nota} de 5: "))
-    notas.append(nota2)
-    soma += nota2
+for nota in range(1, 6):
+    while True:
+        try:
+            nota2 = float(input(f"Digite a nota {nota} de 5: "))
+            notas.append(nota2)
+            soma += nota2
+            break
+        except ValueError:
+            print("Digite apenas números!")
 
 for nota in range(5):
-    print("Nota %d: %.2f" % (nota+1, notas[nota]))
+    print("Nota %d: %.2f" % (nota + 1, notas[nota]))
 
-print("A Média é %.2f" % (soma/5))
+print("A Média é %.2f" % (soma / 5))
